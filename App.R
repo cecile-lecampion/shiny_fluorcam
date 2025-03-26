8cfc3791fc18c8faf1ae955deaf73cbfb18719cb
cecile-lecampion <cecile.lecampion@univ-amu.fr>
2025-03-26 14:24:57 +0100

    initial commit

--- App.R
+++ App.R
@@ -26,6 +26,13 @@ library(RColorBrewer) # to define colors
 library(rstatix)  # for statistical test
 library(rcompanion) # to compute confidence interval for non parametric data
 library(forcats) # for fct_reorder
+library(Rmisc) # for the command summarySE
+library(plyr) # rmisc dependency
+library(multcompView) # for cld letters
+library(ggplot2) # for plotting
+library(dplyr) # for data manipulation
+library(ggbeeswarm) # for geom_quasirandom
+library(zip) # to create zip files
 
 # Définition de la fonction pour vérifier la normalité
 check_normality <- function(shapiro_df) {
@@ -33,7 +40,7 @@ check_normality <- function(shapiro_df) {
   flag_normal <- TRUE
   
   for (i in 1:nrow(shapiro_df)) {
-    if (shapiro_df$p.value[i] <= 0.05) {
+    if (shapiro_df$p[i] <= 0.05) {
       # If any data group does not follow a normal law, stop and flag as non-normal
       flag_normal <- FALSE
       break
@@ -92,7 +99,8 @@ ui <- fluidPage(
       colourInput("point_color", "Point color", value = "darkgreen"),
       uiOutput("columnSelect"),
       actionButton("start_analysis", "Start Analysis"),
-      textOutput("normality_result")
+      downloadButton("download_parametric", "Download All Analysis Results for parametric data"),
+      downloadButton("download_non_parametric", "Download All Analysis Results for non-parametric data")
     ),
     
     mainPanel(
@@ -102,6 +110,7 @@ ui <- fluidPage(
       uiOutput("toggle_button"), # Afficher le bouton ici
       tags$hr(),
       textOutput("selectedValue"),
+      verbatimTextOutput("normality_result"),
       plotOutput("plot_result")
     )
   )
@@ -206,7 +215,7 @@ server <- function(input, output, session) {
       req(result_df$data)
       actionButton("toggle_table", "Show/Hide Full Table")
     })
-  
+    
     # Stocker l'état du bouton
     show_full_table <- reactiveValues(full = FALSE)
     
@@ -237,17 +246,6 @@ server <- function(input, output, session) {
     req(input$column)
     input$column
   })
-  
-  # # Utilisation de VALUE dans l'analyse
-  # observe({
-  #   req(VALUE())
-  #   print(paste("La valeur de la variable VALUE est:", VALUE()))
-  #   
-  #   if (!is.null(result_df$data)) {
-  #     selected_column_values <- result_df$data[, VALUE()]
-  #     print(selected_column_values)
-  #   }
-  # })
   
   output$var2_order_ui <- renderUI({
     req(result_df$data)
@@ -300,11 +298,11 @@ server <- function(input, output, session) {
     
     # Calcul du test de Shapiro
     shapiro_result <- reactive({
-      req(var1(), var2(), MEASURE_COL())
+      req(var1, var2, MEASURE_COL)
       
-      shapiro_df <- result_df %>%
-        group_by(!!sym(var2()), !!sym(var1())) %>%
-        shapiro_test(!!sym(MEASURE_COL()))
+      shapiro_df <- result_df$data %>%
+        group_by(!!sym(var2), !!sym(var1)) %>%
+        shapiro_test(!!sym(MEASURE_COL))
       
       return(shapiro_df)
     })
@@ -318,15 +316,218 @@ server <- function(input, output, session) {
     
     # Affichage du résultat
     output$normality_result <- renderText({
-      if (normality_check()) {
-        "Les données suivent une loi normale."
-      } else {
-        "Les données ne suivent pas une loi normale."
-      }
+        if (normality_check()) {
+          "Datas follow a normal law."
+        } else {
+          "Datas don't follow a normal law."
+        }
+  })
+ 
+  
+  # Analyse des données selon le statut de normalité
+  if (normality_check()) {
+    # Summary
+    library(plyr)
+    library(Rmisc)
+    my_summary <- summarySE(result_df$data, measurevar = MEASURE_COL, groupvars = c(var2, var1))
+    
+    # Stats
+    anova_result <- result_df$data %>%
+      group_by(!!sym(var1)) %>%
+      rstatix::anova_test(formula = reformulate(var2, MEASURE_COL))
+    
+    # Tukey HSD Test
+    tukey_results <- result_df$data %>%
+      group_by(!!sym(var1)) %>% 
+      rstatix::tukey_hsd(as.formula(paste(MEASURE_COL, "~", var2)))
+    
+    # CLD letters for parametric test
+    cld_table_parametric <- tukey_results %>%
+      group_by(!!sym(var1)) %>%
+      summarise(
+        cld = list(multcompView::multcompLetters(setNames(p.adj, paste(group1, group2, sep = "-")), Letters = letters)$Letters),
+        .groups = 'drop'
+      ) %>%
+      unnest_longer(cld) %>%
+      mutate(!!var2 := names(cld))
+    
+    # Plotting
+    df2 <- merge(my_summary, cld_table_parametric, by.x = c(var2, var1), by.y = c(var2, var1))
+    df2[[var1]] <- factor(df2[[var1]], levels = input$var1_order)
+    df2[[var2]] <- factor(df2[[var2]], levels = input$var2_order)
+    result_df$data[[var1]] <- factor(result_df$data[[var1]], levels = input$var1_order)
+    result_df$data[[var2]] <- factor(result_df$data[[var2]], levels = input$var2_order)
+    
+    p <- df2 %>%
+      mutate(
+        !!sym(var1) := as.factor(!!sym(var1)),
+        !!sym(var2) := as.factor(!!sym(var2))
+      ) %>%
+      ggplot(aes(x = !!sym(var2), y = !!sym(MEASURE_COL), fill = !!sym(var2))) +
+      geom_col(color = line_color(), width = 0.6, position = position_dodge2(padding = 0.05)) +
+      scale_fill_manual(values = rep(fill_color(), length(unique(df2[[var2]])))) +
+      scale_y_continuous(
+        expand = expansion(mult = c(0, 0.1)), 
+        breaks = seq(0, 1, by = 0.2))  +
+      geom_quasirandom(data = result_df$data, aes(x = !!sym(var2), y = !!sym(MEASURE_COL)), color = point_color(), width = 0.3, alpha = 0.6) +
+      geom_segment(aes(x = !!sym(var2), xend = !!sym(var2), y = pmax(0, !!sym(MEASURE_COL) - ci), yend = !!sym(MEASURE_COL) + ci), color = "black") +
+      geom_text(aes(x = !!sym(var2), y = !!sym(MEASURE_COL) + (0.15 * max(df2[[MEASURE_COL]], na.rm = TRUE)), label = cld), size = 3, inherit.aes = TRUE) +
+      theme_classic() +
+      theme(
+        axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
+        panel.grid.minor.y = element_blank(),
+        axis.line.x = element_line(linewidth = 0.5),
+        axis.line.y = element_line(linewidth = 0.5),
+        panel.grid = element_blank(),
+        panel.background = element_rect(fill = 'transparent', color = NA),
+        plot.background = element_rect(fill = 'transparent', color = NA),
+        axis.text.y = element_text(vjust = 1),
+        legend.position = "none",
+        strip.background = element_blank(),
+        strip.placement = "outside",
+        strip.text = element_text(face = "plain", size = 10, color = "black", hjust = 0.5)) +
+      facet_wrap(as.formula(paste("~", var1)), nrow = 1) +
+      labs(x = "Treatment", y = "Median Value")
+    
+    output$plot_result <- renderPlot({
+      print(p)
     })
+    
+    ggsave("plot_para.svg", width = 8, height = 6)
+    
+    # Enregistrement de tous les tableaux en tant que fichier ZIP pour les données paramétriques
+    output$download_parametric <- downloadHandler(
+      filename = function() {
+        paste("parametric_analysis_results_", Sys.Date(), ".zip", sep = "")
+      },
+      content = function(file) {
+        temp_dir <- tempdir()
+        shapiro_file <- file.path(temp_dir, "shapiro_test_results.csv")
+        write.csv(shapiro_result(), shapiro_file, row.names = FALSE)
+        my_summary_file <- file.path(temp_dir, "summary_results.csv")
+        write.csv(my_summary, my_summary_file, row.names = FALSE)
+        anova_file <- file.path(temp_dir, "anova_results.csv")
+        write.csv(anova_result, anova_file, row.names = FALSE)
+        tukey_file <- file.path(temp_dir, "tukey_results.csv")
+        write.csv(tukey_results, tukey_file, row.names = FALSE)
+        cld_file <- file.path(temp_dir, "cld_table_parametric.csv")
+        write.csv(cld_table_parametric, cld_file, row.names = FALSE)
+        
+        zip::zipr(file, 
+                  files = c(shapiro_file, my_summary_file, anova_file, tukey_file, cld_file), 
+                  compression_level = 9)
+      }
+    )
+    
+  } else {
+    # Summary
+    conf_int <- groupwiseMedian(
+      data = result_df$data,
+      var = MEASURE_COL,
+      group = c(var2, var1),
+      conf = 0.95,
+      R = 5000,
+      percentile = TRUE,
+      bca = FALSE,
+      digits = 3)
+    
+    # Stats
+    kruskal_pval <- result_df$data %>%
+      group_by(!!sym(var1)) %>%
+      kruskal_test(as.formula(paste(MEASURE_COL, "~", var2))) %>%
+      dplyr::select(all_of(var1), p)
+    
+    significant <- any(kruskal_pval$p < 0.05)
+    
+    if (significant) {
+      pval_dunn <- test_dunn(result_df$data, var1, var2, MEASURE_COL)
+      
+      # CLD letters for non-parametric test
+      cld_table_nonparametric <- pval_dunn %>%
+        group_by(!!sym(var1)) %>%
+        summarise(
+          cld = list(multcompView::multcompLetters(setNames(p.adj, paste(group1, group2, sep = "-")), Letters = letters)$Letters),
+          .groups = 'drop'
+        ) %>%
+        unnest_longer(cld) %>%
+        mutate(!!var2 := names(cld))
+      
+      # Plotting
+      df2 <- merge(conf_int, cld_table_nonparametric, by.x = c(var2, var1), by.y = c(var2, var1))
+      df2[[var1]] <- factor(df2[[var1]], levels = input$var1_order)
+      df2[[var2]] <- factor(df2[[var2]], levels = input$var2_order)
+      result_df$data[[var1]] <- factor(result_df$data[[var1]], levels = input$var1_order)
+      result_df$data[[var2]] <- factor(result_df$data[[var2]], levels = input$var2_order)
+      
+      p <- df2 %>%
+        ggplot(aes(x = !!sym(var2), y = Median, fill = !!sym(var2))) +
+        geom_col(color = line_color(), width = 0.6, position = position_dodge2(padding = 0.05)) +
+        scale_fill_manual(values = rep(fill_color(), length(unique(df2[[var2]])))) +
+        scale_y_continuous(
+          expand = expansion(mult = c(0, 0.1)), 
+          breaks = seq(0, 1, by = 0.2) ) +
+        geom_quasirandom(data = result_df$data, aes(x = !!sym(var2), y = !!sym(MEASURE_COL)), color = point_color(), width = 0.3, alpha = 0.6) +
+        geom_segment(aes(x = !!sym(var2), xend = !!sym(var2), y = pmax(0, Percentile.lower), yend = Percentile.upper), color = "black") +
+        geom_text(aes(x = !!sym(var2), y = Percentile.upper + (0.15 * Median), label = cld), size = 3, inherit.aes = TRUE) +
+        theme_classic() +
+        theme(
+          axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
+          panel.grid.minor.y = element_blank(),
+          axis.line.x = element_line(linewidth = 0.5),
+          axis.line.y = element_line(linewidth = 0.5),
+          panel.grid = element_blank(),
+          panel.background = element_rect(fill = 'transparent', color = NA),
+          plot.background = element_rect(fill = 'transparent', color = NA),
+          axis.text.y = element_text(vjust = 1),
+          legend.position = "none",
+          strip.background = element_blank(),
+          strip.placement = "outside",
+          strip.text = element_text(face = "plain", size = 10, color = "black", hjust = 0.5)
+        ) +
+        facet_wrap(as.formula(paste("~", var1)), nrow = 1, scales = "free_y") +
+        labs(x = "Treatment", y = "Median Value")
+      
+      output$plot_result <- renderPlot({
+        print(p)
+      })
+      
+      ggsave("plot_nonpara.svg", width = 8, height = 6)
+      
+    } else {
+      print("Data are not significantly different, the Dunn test was not performed.")
+      
+    }
+  }
+  
+    # Enregistrement de tous les tableaux en tant que fichier ZIP pour les données paramétriques
+    output$download_non_parametric <- downloadHandler(
+      filename = function() {
+        paste("parametric_analysis_results_", Sys.Date(), ".zip", sep = "")
+      },
+      content = function(file) {
+        temp_dir <- tempdir()
+        shapiro_file <- file.path(temp_dir, "shapiro_test_results.csv")
+        write.csv(shapiro_result(), shapiro_file, row.names = FALSE)
+        conf_int_file <- file.path(temp_dir, "confidence_interval_results.csv")
+        write.csv(conf_int, conf_int_file, row.names = FALSE)
+        kruskal_file <- file.path(temp_dir, "kruskal_results.csv")
+        write.csv(kruskal_pval, kruskal_file, row.names = FALSE)
+        dunn_file <- file.path(temp_dir, "dunn_results.csv")
+        write.csv(pval_dunn, dunn_file, row.names = FALSE)
+        cld_file <- file.path(temp_dir, "cld_table_nonparametric.csv")
+        write.csv(cld_table_nonparametric, cld_file, row.names = FALSE)
+        
+        zip::zipr(file, 
+                  files = c(shapiro_file, conf_int_file, kruskal_file, dunn_file, cld_file), 
+                  compression_level = 9)
+      }
+    )
+  })
+  
+  observeEvent(input$start_analysis, {
+    analysis_result()
   })
 }
-
 
 
 shinyApp(ui = ui, server = server)
