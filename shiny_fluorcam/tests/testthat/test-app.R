1b3278d2107e6be3be17a86842cb4ec931495cea
cecile-lecampion <cecile.lecampion@univ-amu.fr>
2025-09-01 17:18:33 +0200

    remove testfiles

--- shiny_fluorcam/tests/testthat/test-app.R
+++
@@ -1,7 +0,0 @@
-test_that("App loads correctly", {
-  expect_true(TRUE)
-})
-
-test_that("App returns expected output", {
-  expect_equal(1 + 1, 2)
-})
--- shiny_fluorcam/tests/testthat/test-server.R
+++
@@ -1,4 +0,0 @@
-test('server logic works correctly', {
-  expect_equal(server_function(input = 1), expected_output)
-  expect_error(server_function(input = invalid_input), "Expected error message")
-})
